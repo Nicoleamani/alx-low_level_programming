1efba1135c1b19d98b052041f2a231b6ce7ce5da
Nicoleamani <nicoleamani419@gmail.com>
2023-02-26 12:31:31 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
 /**
  * main - Prints a "Programming is like building a multilingual puzzle,
  * followed by a newline"
- * return 0.
+ * return: 0 (Success)
  */
 
-int main(void)
-
+int main(void);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- * main -a program that prints a line using the printf funtion
- * Return: 0
+ * main - Prints "with proper grammer, but the outcome is 
+ * a piece of art," followed by new line 
+ * Return: Always 0 (Succeess)
+ *
  */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
