4533b8a29f961f7eb3be0dea114fbaab70ba283b
Nicoleamani <nicoleamani419@gmail.com>
2023-03-01 04:57:46 -0800

    ./5-string_toupper

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _strncpy - copies at most an inputted number
  * of byte from string scr into dest.
@@ -10,6 +9,7 @@
  * Return: a pointer to the resulting string dest.
  *
  */
+
 char *_strncpy(char *dest, char *src, int n)
 
 {
